e71d18fd0f9a7307aee0f372c03b439f713c11c3
Cerlin <cerlin@linkstreet.in>
2016-03-10 13:20:55 +0530

    Added lot of comments in code. Renamed couple  of functions. Added a feature poll link

--- core/app.js
+++ core/app.js
@@ -1,10 +1,18 @@
 var loader = require('../core/loader');
 var isEqual = require('lodash/isEqual');
 
+
+// Setting the default configuration
 var config = {
     "routeFile" : "./routes/routes.json"
 }
 
+/**
+ * Parses the json string
+ *
+ * @param {string} Json string which has to be parsed.
+ * @return {null} or {object} returns null on parse error and object on parse success.
+ */
 var parseJson = function(json){
     try{
         return JSON.parse(json);
@@ -13,6 +21,13 @@ var parseJson = function(json){
     }
 }
 
+/**
+ * Populates the error object with the message and statuscode supplied
+ *
+ * @param {string} Message which has to be assigned inside object
+ * @param {integer} server status code which has to be sent with response
+ * @return {object} Final prepared object with message and status code
+ */
 var getError = function(message, statuscode){
     return {
         "headers" : {
@@ -25,11 +40,26 @@ var getError = function(message, statuscode){
     }
 }
 
+// Exposing the method to outer world
+
 module.exports = function(){
 
+    /**
+        Request object to save the incoming request. All methods inside this context can read and write data into this object.
+    **/
     var request;
+
+    /**
+        Response object to save the outgoing response. All methods inside this context can read and write data into thi object.
+    **/
     var response;
 
+    /**
+     * Method to dispatch the final response.
+     *
+     * @param {object} Data object which has header info, payload and status code
+     * @return {object} Dispatched response object
+     */
     var responseDispatcher = function (data){
         if ( typeof data === "object" && data !== null){
             // Check and set the headers.
@@ -58,7 +88,13 @@ module.exports = function(){
         }
     };
 
-    var configValidator = function(config){
+    /**
+     * Validate the input request header with the header info in config file.
+     *
+     * @param {object} Data object which has header info, payload and status code. Payload and status code will be passed to next payload validator.
+     * @return {object} Dispatched response object
+     */
+    var headerValidator = function(config){
         if (typeof config === "string") {
             config = parseJson(config);
         }
@@ -67,34 +103,41 @@ module.exports = function(){
 
         if(config === null){
             errorMessage = "Route found, but error loading in the corresponding config.";
-        }
+        } else {
 
-        var requestHeaders = request.headers;
+            var requestHeaders = request.headers;
 
-        try{
-            for(var header in config['request']['headers']) {
-                if(typeof requestHeaders[header] === "undefined"){
+            try{
+                for(var header in config['request']['headers']) {
+                    if(typeof requestHeaders[header] === "undefined"){
 
-                    errorMessage = "One or more header(s) are missing";
+                        errorMessage = "One or more header(s) are missing";
 
-                } else if(requestHeaders[header].toLowerCase() !== config['request']['headers'][header].toLowerCase()) {
-                    
-                    errorMessage = "Header mismatch";
+                    } else if(requestHeaders[header].toLowerCase() !== config['request']['headers'][header].toLowerCase()) {
+                        
+                        errorMessage = "Header mismatch";
 
+                    }
                 }
+            } catch(e) {
+                errorMessage = "Schema error in config file";
             }
-        } catch(e) {
-            errorMessage = "Schema error in config file";
         }
 
         if(errorMessage !== null){
             return responseDispatcher(getError(errorMessage, 400));
         }
 
-        return dispatch(request, config);
+        return payloadValidator(request, config);
     }
 
-    var dispatch = function(request, config){
+    /**
+     * Validate the input request payload with the payload in config file.
+     *
+     * @param {object} Data object which has header info, payload and status code.
+     * @return {object} Dispatched response object
+     */
+    var payloadValidator = function(request, config){
 
         var resp = null;
 
@@ -119,20 +162,43 @@ module.exports = function(){
 
     }
 
+    /**
+     * Handler which handles error in file loading.
+     *
+     * @param {string} Error string which explains why the failurehandler is called.
+     * @return {object} Dispatched response object
+     */
     var failureHandler = function(err){
         return responseDispatcher(getError(err, 400));
     }
 
+    /**
+     * Core object which is exposed to outside world. 
+     *   This object has two public methods
+     *      handle(request, response);
+     *      setRouteFile(routes file full path);
+     *
+     */
     var app = {
 
+        /**
+         * Handler which handles the http request and responds
+         *
+         * @param {object} Request object from node http module.
+         * @param {object} Response object from node http module
+         * @return {object} Dispatched response object
+         */
         handle : function (req, res) {
 
+            // Exposing request and response to other methods
             request = req;
             response = res;
+
             request.postdata = null;
 
             var body = "";
 
+            // Event handler to read and save the data when it arrives
             request.on('data', function (data) {
                 body += data;
                 // 1e6 ==== 1 * Math.pow(10, 6) ==== 1 * 1000000 ~~~ 1MB
@@ -141,24 +207,36 @@ module.exports = function(){
                     request.connection.destroy();
                 }
             });
+
+            // Event handler to parse the data since the data stream has ended
             request.on('end', function () {
                 request.postdata = parseJson(body);
                 // Load the routes
                 loader.load(config.routeFile, appHandler, failureHandler);
             });
 
-
+            /**
+             * Handler which gets executed when the route files are loaded.
+             *
+             * @param {object} Route object passed by loader.
+             * @return {object} Dispatched response object
+             */
             var appHandler = function(route){
-                if (typeof route !== "object") {
+
+                // Check and parse if the parameter is string
+                if (typeof route === "string") {
                     route = parseJson(route);
                 }
 
+                // Send error message if the route is not loaded properly. Route can be null if there is any syntax error in routes.json
                 if(route === null){
                     return responseDispatcher(getError("Error loading the routes file", 400));
                 }
 
+                // Cross validate the request with the routes.json and serve the response.
                 if(typeof route[request.method + ":" + request.url] !== "undefined"){
                     if(typeof route[request.method + ":" + request.url]['rule'] !== "undefined"){
+                        // Try to load the appropriate config file based on the route
                         loader.load(route[request.method + ":" + request.url]['rule'], configValidator, failureHandler);
                     } else {
                         return responseDispatcher(null);
@@ -168,6 +246,13 @@ module.exports = function(){
                 }
             }
         },
+
+        /**
+         * Method to set the route file location. Default location is ./routes/routes.json
+         *
+         * @param {string} Full path of the route file.
+         * @return {void}
+         */
         setRouteFile : function (path) {
             config.routeFile = path;
         }
